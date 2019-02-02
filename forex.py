c6043f26d23389640080da0f47052b82e2288aa1
Matěj <mates.sveda@gmail.com>
2019-02-02 17:44:05 +0100

    improved covert function - symbol inputs and outputs, added currencies.json file

--- forex.py
+++ forex.py
@@ -1,5 +1,6 @@
 # Importing modules
 from forex_python.converter import CurrencyRates, CurrencyCodes
+import json
 
 # Creating instances
 currency_rates = CurrencyRates()
@@ -12,11 +13,22 @@ currency_codes = CurrencyCodes()
 
 #print(currency_codes.get_symbol('NOK'))
 
-# Convert function with arguments input_currency, output currency and amount
+# Loading currencies data (used for getting symbols only)
+with open('currencies.json') as file:
+    data = json.load(file)
+
+# Convert function, using forex-python package
 def convert(input_currency, output_currency=None, amount=1):
     if output_currency is None:
         return currency_rates.get_rates(input_currency)
     else:
+        for currency in data:
+            if currency['symbol'] == input_currency:
+                input_currency = currency['cc']
+            if currency['symbol'] == output_currency:
+                output_currency = currency['cc']
         return currency_rates.convert(input_currency, output_currency, amount)
 
-print(convert('EUR',))
+print(convert('€', 'CZK', 100))
+
+
