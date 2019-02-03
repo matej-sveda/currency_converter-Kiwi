5bb67f9546a3ec4cf0b48526fa54a258d2c5006f
Matěj <mates.sveda@gmail.com>
2019-02-03 11:55:56 +0100

    adjusting the convert function

--- forex.py
+++ forex.py
@@ -2,24 +2,20 @@
 from forex_python.converter import CurrencyRates, CurrencyCodes
 import json
 
-# Creating instances
+# Creating instances of classes from forex-python module
 currency_rates = CurrencyRates()
 currency_codes = CurrencyCodes()
 
-# Testing basic forex-python module functions
-#print(currency_rates.get_rates('CZK'))
-#print(currency_rates.get_rate('EUR', 'CZK'))
-#print(currency_rates.convert('EUR', 'CZK', 100))
-
-#print(currency_codes.get_symbol('NOK'))
-
-# Loading currencies data (used for getting symbols only)
-with open('currencies.json') as file:
+# Loading currencies data
+with open('currencies_supported.json') as file:
     data = json.load(file)
 
-# Convert function, using forex-python package
+# Convert function with arguments input_currency, output currency and amount
 def convert(input_currency, output_currency=None, amount=1):
     if output_currency is None:
+        for currency in data:
+            if currency['symbol'] == input_currency:
+                input_currency = currency['cc']
         return currency_rates.get_rates(input_currency)
     else:
         for currency in data:
@@ -29,6 +25,4 @@ def convert(input_currency, output_currency=None, amount=1):
                 output_currency = currency['cc']
         return currency_rates.convert(input_currency, output_currency, amount)
 
-print(convert('€', 'CZK', 100))
-
-
+print(convert('Skr', 'A$'))
