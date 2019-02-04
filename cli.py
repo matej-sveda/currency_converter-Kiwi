4093ab3eff8e3a2f1b18d2d1e472da5bf6c6da0a
Matěj <mates.sveda@gmail.com>
2019-02-04 12:17:10 +0100

    importing api and cli

--- cli.py
+++ cli.py
@@ -1,8 +1,6 @@
-from flask import Flask, request
+import argparse
 import json
 from forex_python.converter import CurrencyRates, CurrencyCodes
-
-app = Flask(__name__)
 
 # Creating instances of classes from forex-python module
 currency_rates = CurrencyRates()
@@ -12,12 +10,7 @@ currency_codes = CurrencyCodes()
 with open('currencies_supported.json') as file:
     data = json.load(file)
 
-@app.route('/convert', methods=['GET'])
-def convert():
-    amount = request.args.get('amount', type=float)
-    input_currency = request.args.get('input_currency')
-    output_currency = request.args.get('output_currency', default=None)
-
+def convert(amount, input_currency, output_currency=None):
     if output_currency is None:
         for currency in data:
             if currency['symbol'] == input_currency:
@@ -52,7 +45,23 @@ def convert():
         }
 
     result_json = json.dumps(result, sort_keys=True)
+    print(result_json)
     return result_json
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    parser = argparse.ArgumentParser(prog='Currency Converter',
+                                     description='''
+                                     --------------------
+                                     DESCRIPTION:
+                                     This tool works good
+                                     --------------------
+                                     ''',
+                                     epilog="Copyrights @MatejSveda",
+                                     add_help=True
+                                     )
+    parser.add_argument('--amount', action="store", type=float, dest="amount", required=True)
+    parser.add_argument('--input_currency', action="store", dest="input_currency", required=True)
+    parser.add_argument('--output_currency', action="store", dest="output_currency")
+
+    arg = parser.parse_args()
+    convert(arg.amount, arg.input_currency, arg.output_currency)
