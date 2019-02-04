7559110fc2947f3a1db98f1cd26e8b77701b1233
Matěj <mates.sveda@gmail.com>
2019-02-04 13:07:31 +0100

    creating converter.py with convert function and importing to api and cli

--- api.py
+++ api.py
@@ -1,58 +1,17 @@
 from flask import Flask, request
-import json
-from forex_python.converter import CurrencyRates, CurrencyCodes
+from converter import convert
 
 app = Flask(__name__)
 
-# Creating instances of classes from forex-python module
-currency_rates = CurrencyRates()
-currency_codes = CurrencyCodes()
-
-# Loading currencies data
-with open('currencies_supported.json') as file:
-    data = json.load(file)
-
 @app.route('/convert', methods=['GET'])
-def convert():
+def get():
+    # Creates arguments
     amount = request.args.get('amount', type=float)
     input_currency = request.args.get('input_currency')
     output_currency = request.args.get('output_currency', default=None)
 
-    if output_currency is None:
-        for currency in data:
-            if currency['symbol'] == input_currency:
-                input_currency = currency['cc']
-        output = currency_rates.get_rates(input_currency)
-
-        result = {
-            "input": {
-                "amount": amount,
-                "currency": input_currency
-            },
-
-            "output": output
-        }
-    else:
-        for currency in data:
-            if currency['symbol'] == input_currency:
-                input_currency = currency['cc']
-            if currency['symbol'] == output_currency:
-                output_currency = currency['cc']
-        output = currency_rates.convert(input_currency, output_currency, amount)
-
-        result = {
-            "input": {
-                "amount": amount,
-                "currency": input_currency
-            },
-
-            "output": {
-                output_currency: output
-            }
-        }
-
-    result_json = json.dumps(result, sort_keys=True)
-    return result_json
+    # Returns convert function from converter.py
+    return convert(amount, input_currency, output_currency)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- cli.py
+++ cli.py
@@ -1,54 +1,12 @@
 import argparse
-import json
-from forex_python.converter import CurrencyRates, CurrencyCodes
+from converter import convert
 
-# Creating instances of classes from forex-python module
-currency_rates = CurrencyRates()
-currency_codes = CurrencyCodes()
-
-# Loading currencies data
-with open('currencies_supported.json') as file:
-    data = json.load(file)
-
-def convert(amount, input_currency, output_currency=None):
-    if output_currency is None:
-        for currency in data:
-            if currency['symbol'] == input_currency:
-                input_currency = currency['cc']
-        output = currency_rates.get_rates(input_currency)
-
-        result = {
-            "input": {
-                "amount": amount,
-                "currency": input_currency
-            },
-
-            "output": output
-        }
-    else:
-        for currency in data:
-            if currency['symbol'] == input_currency:
-                input_currency = currency['cc']
-            if currency['symbol'] == output_currency:
-                output_currency = currency['cc']
-        output = currency_rates.convert(input_currency, output_currency, amount)
-
-        result = {
-            "input": {
-                "amount": amount,
-                "currency": input_currency
-            },
-
-            "output": {
-                output_currency: output
-            }
-        }
-
-    result_json = json.dumps(result, sort_keys=True)
-    print(result_json)
-    return result_json
+def get(amount, input_currency, output_currency=None):
+    # Returns and prints the result of convert function from converter.py
+    return print(convert(amount, input_currency, output_currency))
 
 if __name__ == '__main__':
+    # Creates parser
     parser = argparse.ArgumentParser(prog='Currency Converter',
                                      description='''
                                      --------------------
@@ -59,9 +17,11 @@ if __name__ == '__main__':
                                      epilog="Copyrights @MatejSveda",
                                      add_help=True
                                      )
+    # Creates arguments
     parser.add_argument('--amount', action="store", type=float, dest="amount", required=True)
     parser.add_argument('--input_currency', action="store", dest="input_currency", required=True)
     parser.add_argument('--output_currency', action="store", dest="output_currency")
 
+    # Passes the arguments into get function
     arg = parser.parse_args()
-    convert(arg.amount, arg.input_currency, arg.output_currency)
+    get(arg.amount, arg.input_currency, arg.output_currency)
